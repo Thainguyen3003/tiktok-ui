5a89c7ddf113826712ccfa749e72c22ea88d467b
Thainguyen3003 <nguyennangthai@gmail.com>
2022-05-10 00:00:39 +0700

    xay dung ui phan header 1-2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,8 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -14,10 +16,15 @@ function Header() {
                 </div>
                 <div className={cx('search')}>
                     <input placeholder="Search account and video" spellCheck="false" />
-                    <button className={cx('clear')}>{/* Clear */}</button>
-                    {/* Loading */}
-                    <button className={cx('search-btn')}>{/* Search */}</button>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
                 </div>
+                <div className={cx('actions')}>Actions</div>
             </div>
         </header>
     );
