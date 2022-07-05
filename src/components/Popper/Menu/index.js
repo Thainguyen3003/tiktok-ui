6bee13c116b2f186101a41164c3081f88aaa7f59
Thainguyen3003 <nguyennangthai@gmail.com>
2022-07-05 10:52:07 +0700

    update code

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -2,19 +2,23 @@ import styles from './Menu.module.scss';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import MenuItem from './MenuItem';
 
 const cx = classNames.bind(styles);
 
-function Menu({ children }) {
+function Menu({ children, items = [] }) {
+    const renderItems = () => {
+        return items.map((item, index) => <MenuItem key={index} data={item} />);
+    };
+
     return (
         <Tippy
             interactive
+            delay={[0, 700]}
             placement="bottom-end"
             render={(attrs) => (
-                <div className={cx('content')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper>
-                        <h2>Menu items</h2>
-                    </PopperWrapper>
+                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper className={cx('menu-popper')}>{renderItems()}</PopperWrapper>
                 </div>
             )}
         >
